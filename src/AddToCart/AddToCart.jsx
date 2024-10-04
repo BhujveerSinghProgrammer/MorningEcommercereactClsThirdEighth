b960ce7410691717f943b2b8bcc804c49700efd7
USER <USER@DESKTOP-GAP4S8J>
2024-10-04 18:32:23 +0530

    jhjhj

--- src/AddToCart/AddToCart.jsx
+++ src/AddToCart/AddToCart.jsx
@@ -1,3 +1,18 @@
+//Changes
+//change 1:- 
+// function AddToCart()
+// {
+//   function increment()
+//   {
+//     console.log('increment');
+//   }
+//     return (
+//      <div><button onClick={increment} >Add to Cart</button></div>
+//         )
+// }
+// export default AddToCart;
+
+//change 2:-
 function AddToCart()
 {
   function increment()
