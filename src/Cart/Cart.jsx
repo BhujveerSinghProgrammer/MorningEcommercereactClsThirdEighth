2e46d3f0aba062b8c871b3c6d8b18aaa58020084
USER <USER@DESKTOP-GAP4S8J>
2024-10-07 18:36:28 +0530

    sdddssssd

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,19 +1,19 @@
+
+
 function Cart({cart})
 {
   const cartList=cart? Object.values(cart):[];
-  return  
-<div>cart</div>
-//   (
-//     <div>
-//       <ul>
-//         {cartList.map((item) => {<li key={item.id}>
-//             {item.title} {item.quantity}
-//           </li>
-// })}
-//       </ul>
-//     </div>
-//   );
-
+  return (
+    <div>
+      <ul>
+        {cartList.map((item) => (
+          <li key={item.id}>
+            {item.title} {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Cart;
