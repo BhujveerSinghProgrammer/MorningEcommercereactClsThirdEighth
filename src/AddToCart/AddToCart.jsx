239142aa52da44bc7eb5bcf49a9979ead1690727
USER <USER@DESKTOP-GAP4S8J>
2024-10-07 17:27:18 +0530

    hghghg

--- src/AddToCart/AddToCart.jsx
+++ src/AddToCart/AddToCart.jsx
@@ -13,14 +13,39 @@
 // export default AddToCart;
 
 //change 2:-
-function AddToCart()
+function AddToCart({product,increaseQuantity,decreaseQuantity,cart})
 {
   function increment()
   {
     console.log('increment');
+    increaseQuantity(product);
   }
+
+  function decrease()
+  {
+    console.log('decrement');
+    decreaseQuantity(product);
+  }
+//check item quantity in the cart then show +- button else show the add to cart button.
+const quantity=cart[product.id]?cart[product.id].quantity:0;//check if cart[product.id] is not null(if product is in cart ) then use (cart[product.id].quantity) else use (0)
+if(quantity===0)
+{
     return (
      <div><button onClick={increment} >Add to Cart</button></div>
         )
+}
+else
+   {
+   return (
+    <div>
+     <button onClick={increment}>+</button>
+     <span>{quantity}</span>
+     <button onClick={decrease} >-</button>
+    </div>
+
+
+        )
+   }
+
 }
 export default AddToCart;
--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -11,12 +11,17 @@ import AddToCart from "../AddToCart";
 //  }
 
 
-export default function ProductCard(props) {
+export default function ProductCard({product,increaseQuantity,decreaseQuantity,cart}) {
    return (
     <div className="card">
-       <h2> {props.product.title} </h2>
-       <h4>{props.product.price.value}</h4>
-       <AddToCart/>
+       <h2> {product.title} </h2>
+       <h4>{product.price.value}</h4>
+       <AddToCart
+       product={product}
+      increaseQuantity={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+      cart={cart}
+      />
     </div>
   );
  }
