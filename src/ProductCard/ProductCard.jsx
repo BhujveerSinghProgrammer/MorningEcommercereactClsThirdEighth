4c4e534b203f9763d6124deca97ce3f3f6938125
USER <USER@DESKTOP-GAP4S8J>
2024-10-07 18:51:25 +0530

    fdfddfdfdf

--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -1,18 +1,27 @@
 import "./ProductCard.css"
 
 import AddToCart from "../AddToCart";
+import Rating from "../Rating";
+
 
 export default function ProductCard({product,increaseQuantity,decreaseQuantity,cart}) {
    return (
     <div className="card">
        <h2> {product.title} </h2>
        <h4>{product.price.value}</h4>
+
+       <Rating/>
+
+
        <AddToCart
        product={product}
       increaseQuantity={increaseQuantity}
       decreaseQuantity={decreaseQuantity}
       cart={cart}
       />
+
+
+
     </div>
   );
  }
