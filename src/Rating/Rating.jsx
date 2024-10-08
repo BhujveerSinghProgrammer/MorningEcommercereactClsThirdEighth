f6c8216011b718610b79223a0b7ccbe6879c4fb0
USER <USER@DESKTOP-GAP4S8J>
2024-10-08 14:58:59 +0530

    gfgfggfgfgfg

--- src/Rating/Rating.jsx
+++ src/Rating/Rating.jsx
@@ -1,17 +1,41 @@
 import Icon from "@mdi/react";
 import {mdiStar} from "@mdi/js";
 //these libraries are like font awesome:-
+import "./Rating.css";
+import classNames from "classnames";
 
-function Rating({rating,maxrating})
-{
+function Rating({ rating, maxrating }) {
   return (
     <div>
-    <Icon path={mdiStar}/>
-    {rating}/{maxrating}
+      {new Array(maxrating).fill(0).map((_, index) => {
+        const isActive = rating >= index + 1;
+        // [true, true, true, true, false]
+        return (
+          <Icon
+            path={mdiStar}
+            size={1.2}
+            key={index}
+            className={classNames("rating__star", {
+              "rating__star--active": isActive
+            })}
+          />
+        );
+      })}
     </div>
-    );
 
+  
+  );
 }
 
-export default Rating;
-//it will be implemented in ProductCard.
+export default Rating;//it will be implemented in ProductCard.
+
+// map on rating and display filled stars
+// map on maxRating - rating to display empty stars
+
+// for loops
+// rating
+// maxrating-rating
+
+// * * * * // *
+// rating // max rating
+// **** // another map *
