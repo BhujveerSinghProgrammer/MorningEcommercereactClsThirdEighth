85384e3d7aaca8f0086541cce2088941ba2a66b0
USER <USER@DESKTOP-GAP4S8J>
2024-10-15 18:28:25 +0530

    ghhghgh

--- src/store.js
+++ src/store.js
@@ -11,22 +11,66 @@ import { legacy_createStore as createStore } from 'redux';
 import { omit } from "lodash";
 
 function cartReducers(state = { items: {} }, action) {
-  switch (action.type) 
-  {
-    case "ADD_TO_CART":{
+ switch (action.type) {
+    // increase quantity of a particular item
+    // add that item into the cart
+
+    case "ADD_TO_CART": {
       console.log(action.payload);
-      return state;
+      const product = action.payload;
+      if (state.items[product.id]) {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...state.items[product.id],
+              quantity: state.items[product.id].quantity + 1
+            }
+          }
+        };
+      } else {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...product,
+              quantity: 1
+            }
+          }
+        };
+      }
     }
 
-   case "REMOVE_FROM_CART":{
+    case "REMOVE_FROM_CART": {
       console.log(action.payload);
-      return state;
+      const product = action.payload;
+      console.log(state);
+      if (state.items[product.id].quantity <= 1) {
+        return {
+          ...state,
+          items: omit(state.items, [product.id])
+        };
+      } else {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...state.items[product.id],
+              quantity: state.items[product.id].quantity - 1
+            }
+          }
+        };
+      }
     }
-   default: return state;
 
+    default:
+      return state;
+  }
+}
 
-  }
-  }
 
   const store=createStore(cartReducers);
    export default store;
