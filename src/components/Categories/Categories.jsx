1c90f8cffa788f9aba8809b060da885b9c5d47cb
USER <USER@DESKTOP-GAP4S8J>
2024-10-16 18:23:25 +0530

    asassaas

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,37 +1,55 @@
-// import { useEffect, useState } from "react";
 
-// const categories = [
-//   { id: 1, name: "Mobile" },
-//   { id: 2, name: "Laptops" },
-//   { id: 3, name: "Clothes" },
-// ];
+//Here useEffect comes into picture,useEffect says that i will not call the api again and again.(while using setTimeOut etc),i will call when the array will be changed.
 
-// function GetProductsApi(callback) {
-//   console.log('Hello 1');
-//   setTimeout(() => {
+import {useEffect, useState} from "react";//step 1  //its nameed export,multiple other things returning from react 
 
-//     callback(categories); // Mock delay of API
-//   }, 1000);
-// }
+const products = [
+  { id: 1, name: "Mobile" },
+  { id: 2, name: "Laptops" },
+  { id: 3, name: "Clothes" },
+];
 
-// export default function Categories() {
-//   let [categories, setCategories] = useState([]);
 
-//   console.log("API call started");
-  
-//   useEffect(() => {
-//     GetProductsApi((resp) => {
-//       setCategories(resp);
-//     });
-//   }, []);
+function GetProductsApi(callback)
+{
+  console.log("api called");
+  setTimeout(() => {
+    callback(products); //
+  }, 2000); //mock delay of api which is 5000 mili second.
+}
 
-//   return (
-//     <div>
-//       <ul>
-//         {categories.map((item) => (
-//           <li key={item.id}>{item.name}</li> // Correctly returning the <li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+//Api call to get the data 
+export default function Categories() {
+
+  // let gp=[];//state var
+    //useState(default Value) returns [stateVar, setterFn] thatswhy we are using let [gp,setGp]=useState([]);
+    let [gp,setGp]=useState([]); //step 2 useState(default Value),default value was empty array (let gp=[];)
+ //Note :- it returns return [stateVar, setterFn] (let [gp,setGp]),here gp was stateVar and we made setterFn=setGp
+
+
+console.log("api call started");
+useEffect(
+function()
+{
+     GetProductsApi(
+     function(resp)
+     {
+      setGp(resp) //step 3,it works like setGp(gp=resp and rerender() the function Products())
+     }
+     );
+}
+,[gp]
+) //useEffect says if the gp is changed then call GetProductsApi again,it called two times ,first on empty gp and second on filled gp.,if gp will change again then it will again call this.
+ 
+  return (
+    <div>
+      <ul>
+        {gp.map((item) => (
+          <li key={item.id}>{item.name}</li> // Correctly returning the <li>
+        ))}
+      </ul>
+    </div>
+
+  );
+}
+
